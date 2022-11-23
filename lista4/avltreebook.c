9e3feb1a297a32fb2c0108e9d5de1de507d34cb8
Aleatoriu <alexandreaparecido@alunos.utfpr.edu.br>
2022-11-23 12:39:28 -0300

    grafos

--- lista4/avltreebook.c
+++ lista4/avltreebook.c
@@ -3,20 +3,117 @@
 #include <string.h>
 #include <stdbool.h>
 
+typedef struct avl{
+    int chave;
+    int altura;
+    struct avl *esq;
+    struct avl *dir;
+}avl;
 
-
-
-
-FILE openArq(char *nomeArq) {
-    FILE *arq = fopen(nomeArq, "r");
-    if (arq == NULL) {
-        printf("Erro ao abrir o arquivo %s no modo %s \n", nomeArq);
-        exit(1);
-    }
-    return arq;
+int max(int a, int b){
+    return (a > b) ? a : b;
 }
 
-int main(void){
+int altura(avl *a){
+    if(a == NULL) return 0;
+    return a->altura;
+}
+
+avl *criaNo(int chave){
+    avl *novo = (avl*)malloc(sizeof(avl));
+    novo->chave = chave;
+    novo->altura = 1;
+    novo->esq = NULL;
+    novo->dir = NULL;
+    return novo;
+}
+
+avl *rotacaoEsq(avl *a){
+    avl *b = a->dir;
+    avl *t2 = b->esq;
+    b->esq = a;
+    a->dir = t2;
+    a->altura = max(altura(a->esq), altura(a->dir)) + 1;
+    b->altura = max(altura(b->esq), altura(b->dir)) + 1;
+    return b;
+}
+
+avl *rotacaoDir(avl *a){
+    avl *b = a->esq;
+    avl *t2 = b->dir;
+    b->dir = a;
+    a->esq = t2;
+    a->altura = max(altura(a->esq), altura(a->dir)) + 1;
+    b->altura = max(altura(b->esq), altura(b->dir)) + 1;
+    return b;
+}
+
+
+int fatorBalanceamento(avl *a){
+    if(a == NULL) return 0;
+    return altura(a->esq) - altura(a->dir);
+}
+
+avl *insere(avl *a, int chave){
+    if(a == NULL) return criaNo(chave);
+    if(chave < a->chave) a->esq = insere(a->esq, chave);
+    else if(chave > a->chave) a->dir = insere(a->dir, chave); 
+    else return a;
+    a->altura = 1 + max(altura(a->esq), altura(a->dir));
+    int fb = fatorBalanceamento(a);
+    if(fb > 1 && chave < a->esq->chave) return rotacaoDir(a);
+    if(fb < -1 && chave > a->dir->chave) return rotacaoEsq(a);
+    if(fb > 1 && chave > a->esq->chave){
+        a->esq = rotacaoEsq(a->esq);
+        return rotacaoDir(a);
+    }
+    if(fb < -1 && chave < a->dir->chave){
+        a->dir = rotacaoDir(a->dir);
+        return rotacaoEsq(a);
+    }
+    return a;
+}
+
+void preOrdem(avl *a){
+    if(a != NULL){
+        printf("%d ", a->chave);
+        preOrdem(a->esq);
+        preOrdem(a->dir);
+    }
+}
+
+void emOrdem(avl *a){
+    if(a != NULL){
+        emOrdem(a->esq);
+        printf("%d ", a->chave);
+        emOrdem(a->dir);
+    }
+}
+
+void posOrdem(avl *a){
+    if(a != NULL){
+        posOrdem(a->esq);
+        posOrdem(a->dir);
+        printf("%d ", a->chave);
+    }
+}
+
+int main(){
+    avl *a = NULL;
+    int n, chave;
+    scanf("%d", &n);
+    for(int i = 0; i < n; i++){
+        scanf("%d", &chave);
+        a = insere(a, chave);
+    }
+    preOrdem(a);
+    printf("
+");
+    emOrdem(a);
+    printf("  ");
+    
+
+/*int main(void){
     FILE *arq;
     char temp1[23], temp2[23];
     arq = fopen("alou.xlsx", "r");
--- praticas-aula/grafinho.c
+++ praticas-aula/grafinho.c
@@ -22,6 +22,56 @@ void startG(grafoMatriz *g, int vertices, bool dirigido){
     }
 }
 
+void insertAresta(grafoMatriz *g, int v1, int v2){
+    g->matriz[v1-1][v2-1] = 1;
+        if(!g->dirigido){
+        g->matriz[v2-1][v1-1] = 1;
+    }
+    g->arestas++;
+}
+
+void printMatrizG(grafoMatriz *g){
+    for(int i = 0; i < g->vertices; i++){
+        for(int j = 0; j < g->vertices; j++){
+            printf("%d ", g->matriz[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+bool hasAresta(grafoMatriz *g, int v1, int v2){
+    return(g->matriz[v1-1][v2-1] == 1);
+}
+
+void showArestas(grafoMatriz *g){
+    for(int i = 0; i < g->vertices; i++){
+        for(int j = 0; j < g->vertices; j++){
+            if(g->matriz[i][j] == 1){
+                if(!g->dirigido) printf(" \n < %d , %d > ", i+1, j+1);
+                else printf(" \n ( %d , %d ) ", i+1, j+1);
+            }
+        }
+    }
+}
+
+void removeAresta(grafoMatriz *g, int v1, int v2){
+    g->matriz[v1-1][v2-1] = 0;
+    if(!g->dirigido){
+        g->matriz[v2-1][v1-1] = 0;
+    }
+    g->arestas--;
+}
+
+void buscaProfundidade(grafoMatriz *g, int v, bool *visitados){
+    visitados[v-1] = true;
+    printf("%d ", v);
+    for(int i = 0; i < g->vertices; i++){
+        if(g->matriz[v-1][i] == 1 && !visitados[i]){
+            buscaProfundidade(g, i+1, visitados);
+        }
+    }
+}
+
 void freeG(grafoMatriz *g){
     for(int i = 0; i < g->vertices; i++){
         free(g->matriz[i]);
@@ -32,7 +82,18 @@ void freeG(grafoMatriz *g){
 int main(){
     grafoMatriz grafo;
 
-    startG(&grafo, 5, false);
+    startG(&grafo, 5, true);
+    insertAresta(&grafo, 1, 2);
+    insertAresta(&grafo, 1, 3);
+    insertAresta(&grafo, 2, 3);
+    insertAresta(&grafo, 2, 5);
+    insertAresta(&grafo, 3, 4);
+    insertAresta(&grafo, 4, 5);
+    printMatrizG(&grafo);
+    showArestas(&grafo);
+    printf(" \n ");
+    buscaProfundidade(&grafo, 1, (bool*)calloc(grafo.vertices, sizeof(bool)));
+    hasAresta(&grafo, 1, 2) ? printf(" \n Tem aresta ") : printf(" \n Nao tem aresta ");
     freeG(&grafo);
 
 
