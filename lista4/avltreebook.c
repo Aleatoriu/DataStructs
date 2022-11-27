70f98c2cb461b04b5ae79cbc8def06d01e9e34d2
Aleatoriu <alexandreaparecido@alunos.utfpr.edu.br>
2022-11-27 17:11:06 -0300

    PEGA ESSE KRLH FDP

--- lista4/avltreebook.c
+++ lista4/avltreebook.c
@@ -3,122 +3,218 @@
 #include <string.h>
 #include <stdbool.h>
 
-typedef struct avl{
-    int chave;
-    int altura;
-    struct avl *esq;
-    struct avl *dir;
-}avl;
+typedef struct node {
+    char *key; 
+    int page;   
+    int height; 
+    struct node *left; 
+    struct node *right;
+} nodeAVL;
 
-int max(int a, int b){
-    return (a > b) ? a : b;
+
+nodeAVL *initTree(){
+    return NULL;
 }
 
-int altura(avl *a){
-    if(a == NULL) return 0;
-    return a->altura;
+bool isEmpty(nodeAVL **tree){
+    return (*tree) == NULL;
 }
 
-avl *criaNo(int chave){
-    avl *novo = (avl*)malloc(sizeof(avl));
-    novo->chave = chave;
-    novo->altura = 1;
-    novo->esq = NULL;
-    novo->dir = NULL;
-    return novo;
+int height(nodeAVL *node){
+    if(node == NULL)
+        return 0;
+    return node->height;
 }
 
-avl *rotacaoEsq(avl *a){
-    avl *b = a->dir;
-    avl *t2 = b->esq;
-    b->esq = a;
-    a->dir = t2;
-    a->altura = max(altura(a->esq), altura(a->dir)) + 1;
-    b->altura = max(altura(b->esq), altura(b->dir)) + 1;
-    return b;
+int refreshHeight(nodeAVL *node){
+    if(node == NULL)
+        return 0;
+    int heightLeft = height(node->left);
+    int heightRight = height(node->right);
+    if(heightLeft > heightRight)
+        return heightLeft + 1;
+    else
+        return heightRight + 1;
 }
 
-avl *rotacaoDir(avl *a){
-    avl *b = a->esq;
-    avl *t2 = b->dir;
-    b->dir = a;
-    a->esq = t2;
-    a->altura = max(altura(a->esq), altura(a->dir)) + 1;
-    b->altura = max(altura(b->esq), altura(b->dir)) + 1;
-    return b;
+void rotateLeft(nodeAVL **node){
+    nodeAVL *aux = (*node)->right;
+    (*node)->right = aux->left;
+    aux->left = (*node);
+    (*node)->height = refreshHeight(*node);
+    aux->height = refreshHeight(aux);
+    (*node) = aux;
 }
 
-
-int fatorBalanceamento(avl *a){
-    if(a == NULL) return 0;
-    return altura(a->esq) - altura(a->dir);
+void rotateRight(nodeAVL **node){
+    nodeAVL *aux = (*node)->left;
+    (*node)->left = aux->right;
+    aux->right = (*node);
+    (*node)->height = refreshHeight(*node);
+    aux->height = refreshHeight(aux);
+    (*node) = aux;
 }
 
-avl *insere(avl *a, int chave){
-    if(a == NULL) return criaNo(chave);
-    if(chave < a->chave) a->esq = insere(a->esq, chave);
-    else if(chave > a->chave) a->dir = insere(a->dir, chave); 
-    else return a;
-    a->altura = 1 + max(altura(a->esq), altura(a->dir));
-    int fb = fatorBalanceamento(a);
-    if(fb > 1 && chave < a->esq->chave) return rotacaoDir(a);
-    if(fb < -1 && chave > a->dir->chave) return rotacaoEsq(a);
-    if(fb > 1 && chave > a->esq->chave){
-        a->esq = rotacaoEsq(a->esq);
-        return rotacaoDir(a);
-    }
-    if(fb < -1 && chave < a->dir->chave){
-        a->dir = rotacaoDir(a->dir);
-        return rotacaoEsq(a);
-    }
-    return a;
+void rotateLeftRight(nodeAVL **node){
+    rotateLeft(&(*node)->left);
+    rotateRight(node);
 }
 
-void preOrdem(avl *a){
-    if(a != NULL){
-        printf("%d ", a->chave);
-        preOrdem(a->esq);
-        preOrdem(a->dir);
-    }
+void rotateRightLeft(nodeAVL **node){
+    rotateRight(&(*node)->right);
+    rotateLeft(node);
 }
 
-void emOrdem(avl *a){
-    if(a != NULL){
-        emOrdem(a->esq);
-        printf("%d ", a->chave);
-        emOrdem(a->dir);
+int balanceFactor(nodeAVL *node){
+    if(node == NULL)
+        return 0;
+    return height(node->left) - height(node->right);
+}
+
+void balance(nodeAVL **node){
+    if(balanceFactor(*node) == 2){
+        if(balanceFactor((*node)->left) >= 0){
+            rotateRight(node);
+        }else{
+            rotateLeftRight(node);
+        }
+    }else if(balanceFactor(*node) == -2){
+        if(balanceFactor((*node)->right) <= 0){
+            rotateLeft(node);
+        }else{
+            rotateRightLeft(node);
+        }
     }
 }
 
-void posOrdem(avl *a){
-    if(a != NULL){
-        posOrdem(a->esq);
-        posOrdem(a->dir);
-        printf("%d ", a->chave);
+void insert(nodeAVL **tree, char *key, int page){
+    if((*tree) == NULL){
+        (*tree) = (nodeAVL*) malloc(sizeof(nodeAVL));
+        (*tree)->key = (char*) malloc((strlen(key) + 1) * sizeof(char));
+        strcpy((*tree)->key, key);
+        (*tree)->page = page;
+        (*tree)->height = 1;
+        (*tree)->left = NULL;
+        (*tree)->right = NULL;
+    }else{
+        if(strcmp(key, (*tree)->key) < 0){
+            insert(&(*tree)->left, key, page);
+        }else if(strcmp(key, (*tree)->key) > 0){
+            insert(&(*tree)->right, key, page);
+        }else{
+            printf("Chave ja existente na arvore AVL de livros de pagina %d e %d para %s e %s respectivamente (ignorando a nova insercao).\n ", (*tree)->page, page, (*tree)->key, key);
+            return;
+        }
+    }
+    (*tree)->height = refreshHeight(*tree);
+    balance(tree);
+}
+
+void preOrder(nodeAVL *tree){
+    if(tree != NULL){
+        printf("%s %d " , tree->key, tree->page);
+        preOrder(tree->left);
+        preOrder(tree->right);
     }
 }
 
-int main(){
-    avl *a = NULL;
-    int n, chave;
-    scanf("%d", &n);
-    for(int i = 0; i < n; i++){
-        scanf("%d", &chave);
-        a = insere(a, chave);
-    }
-    preOrdem(a);
-    printf("
-");
-    emOrdem(a);
-    printf("  ");
-    
-
-/*int main(void){
-    FILE *arq;
-    char temp1[23], temp2[23];
-    arq = fopen("alou.xlsx", "r");
-    while(!feof(arq)){
-        fscanf(arq,"%s \t %s \n", temp1, temp2);
-        printf("%s \t %s \n", temp1, temp2);
+void inOrder(nodeAVL *tree){
+    if(tree != NULL){
+        inOrder(tree->left);
+        printf("%s %d " , tree->key, tree->page);
+        inOrder(tree->right);
     }
 }
+
+void posOrder(nodeAVL *tree){
+    if(tree != NULL){
+        posOrder(tree->left);
+        posOrder(tree->right);
+        printf("%s %d " , tree->key, tree->page);
+    }
+}
+
+void search(nodeAVL *tree, char *key){
+    if(tree == NULL){
+        printf("Chave nao encontrada " ) ;
+        return;
+    }
+    if(strcmp(key, tree->key) < 0){
+        search(tree->left, key);
+    }else if(strcmp(key, tree->key) > 0){
+        search(tree->right, key);
+    }else{
+        printf("Chave encontrada na pagina %d " , tree->page);
+    }
+}
+
+void freeTree(nodeAVL *tree){
+    if(tree != NULL){
+        freeTree(tree->left);
+        freeTree(tree->right);
+        free(tree->key);
+        free(tree);
+    }
+}
+
+
+void openFile(nodeAVL **tree, char *fileName){
+    FILE *file;
+    char key[100];
+    int page;
+    file = fopen(fileName, "r");
+    if(file == NULL){
+        printf("Erro ao abrir o arquivo " ) ;
+        return;
+    }
+    while(!feof(file)){
+        fscanf(file,"%s \t %d \n", key, &page);
+        insert(tree, key, page);
+    }
+    fclose(file);
+}
+
+void systemMenu(nodeAVL **tree){
+    int option;
+    char key[100];
+    int page;
+    do{
+        printf("\n\n 1 - Inserir livro na arvore AVL de livros\n" ) ;
+        printf(" 2 - Buscar livro na arvore AVL de livros\n" ) ;
+        printf(" 3 - Imprimir arvore AVL de livros em ordem\n" ) ;
+        printf(" 4 - Sair\n" ) ;
+        scanf("%d", &option);
+        switch(option){
+            case 1:
+                printf("Digite a chave do livro a ser inserido: " ) ;
+                scanf("%s %d", key, &page);
+                insert(tree, key, page);
+                break;
+            case 2:
+                printf("Digite a chave do livro a ser buscado: " ) ;
+                scanf("%s", key);
+                search(*tree, key);
+                break;
+            case 3:
+                inOrder(*tree);
+                break; 
+            case 4:
+                freeTree(*tree);
+                printf("Saindo do programa " ) ;
+                break;
+            default:
+                printf("Opcao invalida " ) ;
+                break;
+        }
+    }while(option != 4);
+}
+
+
+int main(int argc, char *argv[]){
+    nodeAVL *tree = initTree();
+    openFile(&tree, argv[1]);
+    systemMenu(&tree);
+    freeTree(tree);
+    return EXIT_SUCCESS;
+}
+
